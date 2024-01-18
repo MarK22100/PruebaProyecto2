ac0c1d1ea311affcb430c21dc0657710817df7f3
MaxiMorey <maximiliano.morey1@gmail.com>
2024-01-18 20:52:53 -0300

    Se realizo cambios en tipo de imagenes y se agrego botones de ver mas

--- JS/app.js
+++ JS/app.js
@@ -14,25 +14,6 @@ function CrearCards() {
           <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
             Comprar
           </button>
-    
-          <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h1 class="modal-title fs-5" id="exampleModalLabel">¿Desea continuar con la compra?</h1>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  
-                </div>
-                <div class="modal-body">
-                    Presione continuar para seguir con su compra
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-danger" data-bs-dismiss="modal">No</button>
-                  <button type="button" class="btn btn-success">Continuar</button>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
       </div>`
         
