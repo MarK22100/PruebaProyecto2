70b11047e5822a9e7fad92f86ae2462c3229cec9
AgusGuitler <you@example.com>
2024-01-25 09:14:13 -0300

    CRUD: Update

--- js/admin.js
+++ js/admin.js
@@ -71,93 +71,93 @@ ListarProductos();
 let esEdicion = false;
 
 function GuardarProducto(e) {
-e.preventDefault();
-if (
-    validarTodo(
-    inputCodigo,
-    inputNombre,
-    inputDescripcion,
-    inputCategoria,
-    inputStock,
-    inputPrecio,
-    inputImgUrl
-    )
-) {
-    CrearProducto();
-} else {
-    Swal.fire({
-    title: "Ups",
-    text: "Todos los campos son requeridos.",
-    icon: "error",
-    });
-}
+    e.preventDefault();
+    if (
+        validarTodo(
+        inputCodigo,
+        inputNombre,
+        inputDescripcion,
+        inputCategoria,
+        inputStock,
+        inputPrecio,
+        inputImgUrl
+        )
+    ) {
+        CrearProducto();
+    } else {
+        Swal.fire({
+        title: "Ups",
+        text: "Todos los campos son requeridos.",
+        icon: "error",
+        });
+    }
 }
 
 function CrearProducto() {
-console.log('Entró en guardar producto');
-const nuevoProducto = {
-    codigo: inputCodigo.value,
-    nombre: inputNombre.value,
-    descripcion: inputDescripcion.value,
-    categoria: inputCategoria.value,
-    stock: inputStock.value,
-    precio: inputPrecio.value,
-    imgUrl: inputImgUrl.value,
-};
+    console.log('Entró en guardar producto');
+    const nuevoProducto = {
+        codigo: inputCodigo.value,
+        nombre: inputNombre.value,
+        descripcion: inputDescripcion.value,
+        categoria: inputCategoria.value,
+        stock: inputStock.value,
+        precio: inputPrecio.value,
+        imgUrl: inputImgUrl.value,
+    };
 
-arrayProductos.push(nuevoProducto);
-Swal.fire({
-    title: "Éxito",
-    text: "El producto se guardó correctamente",
-    icon: "success",
-});
-LimpiarFormulario();
-ListarProductos();
+    arrayProductos.push(nuevoProducto);
+    Swal.fire({
+        title: "Éxito",
+        text: "El producto se guardó correctamente",
+        icon: "success",
+    });
+    LimpiarFormulario();
+    ListarProductos();
 }
 
 window.LimpiarFormulario = function () {
-form.reset();
-inputCodigo.className = "form-control";
-inputCodigo.value = ObtenerCodigoAleatorio();
-inputNombre.className = "form-control";
-inputDescripcion.className = "form-control";
-inputCategoria.className = "form-control";
-inputStock.className = "form-control";
-inputPrecio.className = "form-control";
-inputImgUrl.className = "form-control";
-GuardarLocalStorage();
+    form.reset();
+    inputCodigo.className = "form-control";
+    inputCodigo.value = ObtenerCodigoAleatorio();
+    inputNombre.className = "form-control";
+    inputDescripcion.className = "form-control";
+    inputCategoria.className = "form-control";
+    inputStock.className = "form-control";
+    inputPrecio.className = "form-control";
+    inputImgUrl.className = "form-control";
+    GuardarLocalStorage();
 };
 
 function GuardarLocalStorage() {
-localStorage.setItem("productos", JSON.stringify(arrayProductos));
+    localStorage.setItem("productos", JSON.stringify(arrayProductos));
 }
 
 function ValidateRole(){
-console.log('Entró en checkAdmin');
-const role=getRoleUserLog();
+    console.log('Entró en checkAdmin');
+    const role=getRoleUserLog();
 
-if(role!=='Admin'){
-    window.location.replace('/index.html');
-};  
+    if(role!=='Admin'){
+        window.location.replace('/index.html');
+    };  
 };
 
 function ListarProductos() {
-bodyTabla.innerHTML = "";
-arrayProductos.forEach((element) => {
-    bodyTabla.innerHTML += ` <tr>                  
-        <th scope="row">${element.codigo}</th>
-        <td>${element.nombre}</td>
-        <td>${element.descripcion}</td>
-        <td>$ ${element.precio}</td>
-        <td><a href="${element.imgUrl}" target="_blank" title="Ver Imagen">${element.imgUrl}</a></td>
-        <td class="">
-        <div class="d-flex">
-        <a href='#titulo' class="btn btn-warning mx-1" onclick="PrepararEdicion('${element.codigo}')">Editar</a>
-        <button type="button" class="btn btn-danger mx-1" onclick="BorrarProducto('${element.codigo}')" >Eliminar</button>
-        </div>
-        </td>                
-    </tr>`;
-});
+    bodyTabla.innerHTML = "";
+    arrayProductos.forEach((element) => {
+        bodyTabla.innerHTML += ` <tr>                  
+            <th scope="row">${element.codigo}</th>
+            <td>${element.nombre}</td>
+            <td>${element.descripcion}</td>
+            <td>$ ${element.precio}</td>
+            <td><a href="${element.imgUrl}" target="_blank" title="Ver Imagen">${element.imgUrl}</a></td>
+            <td class="">
+            <div class="d-flex">
+            <a href='#titulo' class="btn btn-warning mx-1" onclick="PrepararEdicion('${element.codigo}')">Editar</a>
+            <button type="button" class="btn btn-danger mx-1" onclick="BorrarProducto('${element.codigo}')" >Eliminar</button>
+            </div>
+            </td>                
+        </tr>`;
+    });
 }
 
 ValidateRole();
