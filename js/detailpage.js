295303f4578a8ad9f169c82dc67d9cffba31bbb9
matiasmed <medinamatias63c1@gmail.com>
2024-01-28 18:22:42 -0300

    intento de configuracion del js

--- js/detailpage.js
+++ js/detailpage.js
@@ -1,4 +1,5 @@
-let arrayProduct = JSON.parse(localStorage.getItem("products")) || [];
+const arrFavoriteProducts = JSON.parse(localStorage.getItem('favoriteProducts'));
+const arrayProducts = JSON.parse(localStorage.getItem("products")) || [];
 
 let title = document.getElementById('title');
 let category = document.getElementById('category');
@@ -6,24 +7,47 @@ let image = document.getElementById('image');
 let price = document.getElementById('price');
 let description = document.getElementById('description');
 let stock = document.getElementById('stock');
+let addFavorite = document.getElementById('addFavorite');
 
-function detailPage(){
-    const arrayProducts = JSON.parse(localStorage.getItem("products")) || [];
 
-    let productId = new URLSearchParams(window.location.search).get('code');
-    const prodSelec = arrayProducts.filter((element) =>{
+let productId = new URLSearchParams(window.location.search).get('codigo');
+
+function detailPage() {
+    const prodSelec = arrayProducts.filter((element) => {
         return element.code == productId
     })[0]
 
-    if(!prodSelec){
-        window.location.href="/index.html"
+    if (!prodSelec) {
+        window.location.href = "/index.html"
     }
-    title.innerText = prodSelec.title;
+    title.innerText = prodSelec.name;
     category.innerText = prodSelec.category;
-    image.innerText = prodSelec.image;
+    image.src = prodSelec.imgUrl;
     price.innerText = prodSelec.price;
     description.innerText = prodSelec.description;
     stock.innerText = prodSelec.stock;
 }
 
 detailPage();
+
+
+window.addFavProduct = function() {
+
+    const favoriteProduct = arrayProducts.find((element) => {
+        return element.code === productId;
+    })
+     
+         if (arrFavoriteProducts.includes(favoriteProduct)) {
+             Swal.fire({
+                 title: "Ups??",
+                 text: "Este prodecto ya esta en tu lista de favoritos",
+                 icon: "info"
+             });
+         }
+         else {
+
+            arrFavoriteProducts.push(favoriteProduct);
+            localStorage.setItem('favoriteProducts', JSON.stringify(arrFavoriteProducts));
+
+        }
+}
