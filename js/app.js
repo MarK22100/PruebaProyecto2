f122183d6bb89f0a86a2c957b77fe04ce1e9cdf7
MaxiMorey <maximiliano.morey1@gmail.com>
2024-01-27 23:17:05 -0300

    se corrigio responsive de tarjeta index y aboutus

--- js/app.js
+++ js/app.js
@@ -1,29 +1,29 @@
-let cardProductos = document.getElementById("cardProductos")
-console.log(cardProductos);
+let cardProduct = document.getElementById("cardProduct")
+console.log(cardProduct);
 function CrearCards() {
-    const arrayProductos = JSON.parse(localStorage.getItem("productos")) || [] ; 
-    cardProductos.innerHTML = ""; 
-    arrayProductos.forEach((element) => {
+    const arrayProduct = JSON.parse(localStorage.getItem("product")) || [] ; 
+    cardProduct.innerHTML = ""; 
+    arrayProduct.forEach((element) => {
         cardProductos.innerHTML += `
-        <div class="card mt-5 mx-auto card shadow w-100" style="max-width: 350px;" id="cardProductos" category="${element.categoria}">
-        <img src="${element.imgUrl}" class="card-img-top" style="height: 300px" alt="${element.descripcion}" >
-        <div class="card-body text-center">
-          <h5 class="card-title">${element.nombreProducto}</h5>
-          <p class="card-text">${element.descripcion}</p>
-          <p class="card-text">$${element.precio}</p>
-          <a href="#">
-          <button type="button" class="btn btn-dark btn-custom-hoveer" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            Comprar
-          </button>
-        </a>
-        <a href="#">
-          <button type="button" class="btn btn-danger btn-custom-colours btn-custom-hoveer" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            Ver mas
-          </button>
-          </a>
+        <div class="col mb-4">
+        <div class="card product-item card mx-auto shadow" style="max-width: 350px;" id="cardProduct" category="${element.category}">
+          <img src="${element.url}" class="card-img-top" style="height: 300px" alt="...">
+          <div class="card-body text-center">
+            <h5 class="card-title">${element.name}</h5>
+            <p class="card-text">${element.descripcion}.</p>
+            <p class="card-text">$ ${element.price}</p>
+            <button type="button" class="btn btn-dark btn-custom-hoveer" data-bs-toggle="modal" data-bs-target="#exampleModal">
+              <a href="./pages/errorpage.html">Comprar</a>
+            </button>
+            <a href="#">
+              <button type="button" class="btn btn-danger btn-custom-colours btn-custom-hoveer" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Ver mas
+              </button>
+            </a>
+          </div>
         </div>
-      </div>`
-        
+      </div>
+      `
      });
-}
+    }
 
